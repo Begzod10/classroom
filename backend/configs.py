f99fd4ba34277f9f7341754dc8f33c9eeee1e58e
Begzod10 <rimefara22@gmail.com>
2025-08-06 15:12:20 +0500

    commit

--- backend/configs.py
+++ backend/configs.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api = '/api'
-gennis_server_url = "http://192.168.1.19:5002"
-# gennis_server_url = os.getenv('GENNIS_SERVER_URL')
+gennis_server_url = os.getenv('GENNIS_SERVER_URL')
 
 turon_server_url = os.getenv('TURON_SERVER_URL')
