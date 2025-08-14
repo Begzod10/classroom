5f79300702455447a6e93395a98f6d0269ea671b
shahzodbeeck <shahzodomonboyev1.com>
2025-08-14 20:39:21 +0500

    sf

--- backend/user/change_info.py
+++ backend/user/change_info.py
@@ -1,12 +1,11 @@
-import pprint
-
-from backend.models.basic_model import User, File
-from app import request, jsonify, db, jwt_required, get_jwt_identity
-from werkzeug.security import generate_password_hash, check_password_hash
-from backend.basics.settings import create_msg, edit_msg, del_msg, check_file, check_img_remove, add_file
 import requests
-from backend.configs import gennis_server_url, turon_server_url
 from flask import Blueprint
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import request, jsonify, db, jwt_required, get_jwt_identity
+from backend.basics.settings import edit_msg, check_file, check_img_remove, add_file
+from backend.configs import gennis_server_url, turon_server_url
+from backend.models.basic_model import User, File
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -37,17 +36,18 @@ def change_pas_user():
     type = json['type']
     user = User.query.filter(User.classroom_user_id == indentity).first()
     if type == "info":
-        User.query.filter(User.classroom_user_id == indentity).update({
-            "username": json['username']
-        })
+        User.query.filter(User.classroom_user_id == indentity).update({"username": json['username']})
         db.session.commit()
         if user.system_name == "gennis":
-            response = requests.post(f"{gennis_server_url}/api/student/change_student_classroom/{user.platform_id}", headers={
-                'Content-Type': 'application/json'
-            }, json={
-                "username": json['username'],
-            })
+            response = requests.post(f"{gennis_server_url}/api/student/change_student_classroom/{user.platform_id}",
+                                     headers={'Content-Type': 'application/json'},
+                                     json={"username": json['username'], })
             return jsonify(response.json())
+        else:
+            response = requests.patch(f"{turon_server_url}/api/Users/users/update/{user.turon_id}/",
+                                     headers={'Content-Type': 'application/json'},
+                                     json={"username": json['username'], })
+            return jsonify({"msg": "Foydalanuvchi ma'lumotlari o'zgartirildi"})
 
     else:
         password = json['password']
@@ -55,23 +55,27 @@ def change_pas_user():
         User.query.filter(User.classroom_user_id == indentity).update({'password': hash})
         db.session.commit()
         if user.system_name == "gennis":
-            response = requests.post(f"{gennis_server_url}/api/student/change_student_password/{user.platform_id}", headers={
-                'Content-Type': 'application/json'
-            }, json={
-                "password": password,
-            })
+            response = requests.post(f"{gennis_server_url}/api/student/change_student_password/{user.platform_id}",
+                                     headers={'Content-Type': 'application/json'}, json={"password": password, })
             return jsonify(response.json())
+        else:
+            response = requests.patch(f"{turon_server_url}/api/Users/users/update/{user.turon_id}/",
+                                     headers={'Content-Type': 'application/json'},
+                                     json={"password": json['password'], })
+            return jsonify({"msg": "Foydalanuvchi ma'lumotlari o'zgartirildi"})
 
 
 # @app.route(f'{api}/check_password', methods=['POST'])
-@user_bp.route(f'/check_password', methods=['POST'])
+@user_bp.route('/check_password', methods=['POST'])
 @jwt_required()
 def check_password():
     identity = get_jwt_identity()
     body = {}
-    password = request.get_json()['password']
-    username = User.query.filter_by(classroom_user_id=identity).first()
-    if username and check_password_hash(username.password, password):
+    password = request.get_json().get('password')
+
+    user = User.query.filter_by(classroom_user_id=identity).first()
+
+    if user and user.password and check_password_hash(user.password, password):
         body['password'] = True
     else:
         body['password'] = False
@@ -87,13 +91,10 @@ def check_username():
     username = request.get_json()['username']
     user = User.query.filter_by(classroom_user_id=indentity).first()
     if user.system_name == "gennis":
-        response = requests.post(f"{gennis_server_url}/api/check_exist_username/{user.platform_id}",
-                                 json={
-                                     "username": username
-                                 })
+        response = requests.post(f"{gennis_server_url}/api/checks/check_exist_username/{user.platform_id}",
+                                 json={"username": username})
         return jsonify(response.json())
     else:
-        response = requests.post(f"{turon_server_url}/api/Users/username-check/", json={
-            "username": username
-        })
+        print(turon_server_url)
+        response = requests.post(f"{turon_server_url}/api/Users/username-check/", json={"username": username})
         return jsonify(response.json())
