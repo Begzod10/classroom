8660985e2e74c05661ced27514505b91c28e5c78
thesobirov <shahzodsobirjonov9@gmail.com>
2026-01-26 08:34:55 +0500

    acas

--- backend/configs.py
+++ backend/configs.py
@@ -6,4 +6,5 @@ api = '/api'
 
 gennis_server_url = os.getenv('GENNIS_SERVER_URL')
 
-turon_server_url = os.getenv('TURON_SERVER_URL')
+turon_server_url = "http://26.196.249.247:8000"
+# turon_server_url = os.getenv('TURON_SERVER_URL')
