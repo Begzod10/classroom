21cde21dc6b6df42e4a14511b6938238cccf2de6
thesobirov <shahzodsobirjonov9@gmail.com>
2026-01-26 15:13:42 +0500

    acacrraaaa

--- backend/configs.py
+++ backend/configs.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 api = '/api'
 
-# gennis_server_url = os.getenv('GENNIS_SERVER_URL')
-gennis_server_url = "http://127.0.0.1:5002"
+gennis_server_url = os.getenv('GENNIS_SERVER_URL')
+# gennis_server_url = "http://127.0.0.1:5002"
 
-turon_server_url = "http://26.196.249.247:8000"
-# turon_server_url = os.getenv('TURON_SERVER_URL')
+# turon_server_url = "http://26.196.249.247:8000"
+turon_server_url = os.getenv('TURON_SERVER_URL')
